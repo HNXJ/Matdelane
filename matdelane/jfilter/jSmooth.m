afc3a949ee0e9b0ad686a9e27e987d392f8bc092
Nejat <hamed.nejat@vanderbilt.edu>
2024-09-18 10:08:23 -0500

    V2.5

---
+++ matdelane/jfilter/jSmooth.m
@@ -0,0 +1,35 @@
+function y = jSmooth(x, w)
+
+    y = zeros(size(x));
+    d = ndims(x);
+
+    if d == 2
+
+        bl = mean(x(:, 1:1000), 2);
+        n1 = size(x, 1);
+
+        parfor ik = 1:n1
+
+            y(ik, :) = bl(ik) + detrend(smooth(x(ik, :), w));
+
+        end
+
+    elseif d == 3
+        
+        bl = mean(x(:, :, 1:1000), 3);
+        n1 = size(x, 1);
+        n2 = size(x, 2);
+
+        parfor ik = 1:n1
+
+            for jk = 1:n2
+            
+                y(ik, jk, :) = bl(ik, jk) + detrend(smooth(x(ik, jk, :), w));
+
+            end
+
+        end
+        
+    end
+
+end
