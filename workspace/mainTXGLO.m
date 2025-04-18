3a1172a5660f26d224b761165a546b33a12f1047
Nejat <hamed.nejat@vanderbilt.edu>
2025-04-18 11:15:31 -0500

    V3.10.3

--- workspace/mainTXGLO.m
+++ workspace/mainTXGLO.m
@@ -16,7 +16,7 @@ disp("Setup done.");
 
 %% TFR unifier > LOAD/append
 
-areax = "PFC";
+areax = "FST";
 tfrpath = "tfrData\";
 tfrfiles = {dir(tfrpath).name};
 tfrfiles = tfrfiles(endsWith(tfrfiles, areax + ".mat"));
